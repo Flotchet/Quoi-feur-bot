ef9f18e2a53fd72dc9d7568897a6610c951d169c
Flotchet <flo.maisse@live.be>
2023-01-22 20:55:34 +0100

    update with some fine humor

--- Quoi.py
+++ Quoi.py
@@ -21,6 +21,23 @@ def take_command():
             if 'quoi' in command:
                 engine.say("feur")
                 engine.runAndWait()
+
+            if 'feur' or 'coiffeur' in command:
+                engine.say("bi")
+                engine.runAndWait()
+
+            if 'bi' in command:
+                engine.say("naire")
+                engine.runAndWait()
+
+            if 'naire' or 'binaire'in command:
+                engine.say("nir")
+                engine.runAndWait()
+
+            if 'nir' in command:
+                engine.say("vana")
+                engine.runAndWait()
+                
     except:
         pass
     return command
